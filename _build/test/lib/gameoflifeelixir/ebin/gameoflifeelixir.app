e36f2534b31e1900436a0e08f86dc104f7d181bc
domu904 <domu904@gmail.com>
2018-03-12 14:11:50 -0500

    Added new test

---
+++ _build/test/lib/gameoflifeelixir/ebin/gameoflifeelixir.app
@@ -0,0 +1,7 @@
+{application,gameoflifeelixir,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"gameoflifeelixir"},
+              {modules,['Elixir.GameOfLifeElixir']},
+              {registered,[]},
+              {vsn,"0.1.0"},
+              {extra_applications,[logger]}]}.
--- test/gameoflifeelixir_test.exs
+++ test/gameoflifeelixir_test.exs
@@ -1,8 +1,21 @@
+
+defmodule SendingProcess do
+  def run(pid) do
+    send(pid, :ping)
+  end
+end
+
+
 defmodule GameOfLifeElixirTest do
   use ExUnit.Case
   doctest GameOfLifeElixir
 
   test "greets the world" do
     assert GameOfLifeElixir.hello() == :world
+  end
+
+  test "receives ping" do
+    SendingProcess.run(self())
+    assert_received :ping
   end
 end
